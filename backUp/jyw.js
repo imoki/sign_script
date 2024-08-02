b13348413736c2638131eb462559526886094bc3
codebhc <lxbhcgithub@outlook.com>
2024-08-02 13:33:37 +0800

    Create jyw.js

---
+++ backUp/jyw.js
@@ -0,0 +1,468 @@
+// 菁优网自动签到（修改这里，这里填是什么签到）
+// 20240608 （修改这里）
+
+let sheetNameSubConfig = "jyw"; // 分配置表名称（修改这里，这里填表的名称，需要和UPDATE文件中的一致，自定义的）
+let pushHeader = "【菁优网】";    //（修改这里，这里给自己看的，随便填）
+let sheetNameConfig = "CONFIG"; // 总配置表
+let sheetNamePush = "PUSH"; // 推送表名称
+let sheetNameEmail = "EMAIL"; // 邮箱表
+let flagSubConfig = 0; // 激活分配置工作表标志
+let flagConfig = 0; // 激活主配置工作表标志
+let flagPush = 0; // 激活推送工作表标志
+let line = 21; // 指定读取从第2行到第line行的内容
+var message = ""; // 待发送的消息
+var messageArray = [];  // 待发送的消息数据，每个元素都是某个账号的消息。目的是将不同用户消息分离，方便个性化消息配置
+var messageOnlyError = 0; // 0为只推送失败消息，1则为推送成功消息。
+var messageNickname = 0; // 1为推送位置标识（昵称/单元格Ax（昵称为空时）），0为不推送位置标识
+var messageHeader = []; // 存放每个消息的头部，如：单元格A3。目的是分离附加消息和执行结果消息
+var messagePushHeader = pushHeader; // 存放在总消息的头部，默认是pushHeader,如：【xxxx】
+
+var jsonPush = [
+  { name: "bark", key: "xxxxxx", flag: "0" },
+  { name: "pushplus", key: "xxxxxx", flag: "0" },
+  { name: "ServerChan", key: "xxxxxx", flag: "0" },
+  { name: "email", key: "xxxxxx", flag: "0" },
+  { name: "dingtalk", key: "xxxxxx", flag: "0" },
+  { name: "discord", key: "xxxxxx", flag: "0" },
+]; // 推送数据，flag=1则推送
+var jsonEmail = {
+  server: "",
+  port: "",
+  sender: "",
+  authorizationCode: "",
+}; // 有效邮箱配置
+
+flagConfig = ActivateSheet(sheetNameConfig); // 激活推送表
+// 主配置工作表存在
+if (flagConfig == 1) {
+  console.log("开始读取主配置表");
+  let name; // 名称
+  let onlyError;
+  let nickname;
+  for (let i = 2; i <= 100; i++) {
+    // 从工作表中读取推送数据
+    name = Application.Range("A" + i).Text;
+    onlyError = Application.Range("C" + i).Text;
+    nickname = Application.Range("D" + i).Text;
+    if (name == "") {
+      // 如果为空行，则提前结束读取
+      break; // 提前退出，提高效率
+    }
+    if (name == sheetNameSubConfig) {
+      if (onlyError == "是") {
+        messageOnlyError = 1;
+        console.log("只推送错误消息");
+      }
+
+      if (nickname == "是") {
+        messageNickname = 1;
+        console.log("单元格用昵称替代");
+      }
+
+      break; // 提前退出，提高效率
+    }
+  }
+}
+
+flagPush = ActivateSheet(sheetNamePush); // 激活推送表
+// 推送工作表存在
+if (flagPush == 1) {
+  console.log("开始读取推送工作表");
+  let pushName; // 推送类型
+  let pushKey;
+  let pushFlag; // 是否推送标志
+  for (let i = 2; i <= line; i++) {
+    // 从工作表中读取推送数据
+    pushName = Application.Range("A" + i).Text;
+    pushKey = Application.Range("B" + i).Text;
+    pushFlag = Application.Range("C" + i).Text;
+    if (pushName == "") {
+      // 如果为空行，则提前结束读取
+      break;
+    }
+    jsonPushHandle(pushName, pushFlag, pushKey);
+  }
+  // console.log(jsonPush)
+}
+
+// 邮箱配置函数
+emailConfig();
+
+flagSubConfig = ActivateSheet(sheetNameSubConfig); // 激活分配置表
+if (flagSubConfig == 1) {
+  console.log("开始读取分配置表");
+  for (let i = 2; i <= line; i++) {
+    var cookie = Application.Range("A" + i).Text;
+    var exec = Application.Range("B" + i).Text;
+    if (cookie == "") {
+      // 如果为空行，则提前结束读取
+      break;
+    }
+    if (exec == "是") {
+      execHandle(cookie, i);
+    }
+  }
+
+  message = messageMerge()// 将消息数组融合为一条总消息
+  push(message); // 推送消息
+}
+
+// 将消息数组融合为一条总消息
+function messageMerge(){
+  for(i=0; i<messageArray.length; i++){
+    if(messageArray[i] != "" && messageArray[i] != null)
+    {
+      message += messageHeader[i] + messageArray[i] + " "; // 加上推送头
+    }
+  }
+  if(message != "")
+  {
+    console.log(message)  // 打印总消息
+  }
+  return message
+}
+
+// 总推送
+function push(message) {
+  if (message != "") {
+    message = messagePushHeader + message // 消息头最前方默认存放：【xxxx】
+    let length = jsonPush.length;
+    let name;
+    let key;
+    for (let i = 0; i < length; i++) {
+      if (jsonPush[i].flag == 1) {
+        name = jsonPush[i].name;
+        key = jsonPush[i].key;
+        if (name == "bark") {
+          bark(message, key);
+        } else if (name == "pushplus") {
+          pushplus(message, key);
+        } else if (name == "ServerChan") {
+          serverchan(message, key);
+        } else if (name == "email") {
+          email(message);
+        } else if (name == "dingtalk") {
+          dingtalk(message, key);
+        } else if (name == "discord") {
+          discord(message, key);
+        }
+      }
+    }
+  } else {
+    console.log("消息为空不推送");
+  }
+}
+
+// 推送bark消息
+function bark(message, key) {
+  if (key != "") {
+    let url = "https://api.day.app/" + key + "/" + message;
+    // 若需要修改推送的分组，则将上面一行改为如下的形式
+    // let url = 'https://api.day.app/' + bark_id + "/" + message + "?group=分组名";
+    let resp = HTTP.get(url, {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+    sleep(5000);
+  }
+}
+
+// 推送pushplus消息
+function pushplus(message, key) {
+  if (key != "") {
+    // url = "http://www.pushplus.plus/send?token=" + key + "&content=" + message;
+    url = "http://www.pushplus.plus/send?token=" + key + "&content=" + message + "&title=" + pushHeader;  // 增加标题
+    let resp = HTTP.fetch(url, {
+      method: "get",
+    });
+    sleep(5000);
+  }
+}
+
+// 推送serverchan消息
+function serverchan(message, key) {
+  if (key != "") {
+    url =
+      "https://sctapi.ftqq.com/" +
+      key +
+      ".send" +
+      "?title=消息推送" +
+      "&desp=" +
+      message;
+    let resp = HTTP.fetch(url, {
+      method: "get",
+    });
+    sleep(5000);
+  }
+}
+
+// email邮箱推送
+function email(message) {
+  var myDate = new Date(); // 创建一个表示当前时间的 Date 对象
+  var data_time = myDate.toLocaleDateString(); // 获取当前日期的字符串表示
+  let server = jsonEmail.server;
+  let port = parseInt(jsonEmail.port); // 转成整形
+  let sender = jsonEmail.sender;
+  let authorizationCode = jsonEmail.authorizationCode;
+
+  let mailer;
+  mailer = SMTP.login({
+    host: server,
+    port: port,
+    username: sender,
+    password: authorizationCode,
+    secure: true,
+  });
+  mailer.send({
+    from: pushHeader + "<" + sender + ">",
+    to: sender,
+    subject: pushHeader + " - " + data_time,
+    text: message,
+  });
+  // console.log("已发送邮件至：" + sender);
+  console.log("已发送邮件");
+  sleep(5000);
+}
+
+// 邮箱配置
+function emailConfig() {
+  console.log("开始读取邮箱配置");
+  let length = jsonPush.length; // 因为此json数据可无序，因此需要遍历
+  let name;
+  for (let i = 0; i < length; i++) {
+    name = jsonPush[i].name;
+    if (name == "email") {
+      if (jsonPush[i].flag == 1) {
+        let flag = ActivateSheet(sheetNameEmail); // 激活邮箱表
+        // 邮箱表存在
+        // var email = {
+        //   'email':'', 'port':'', 'sender':'', 'authorizationCode':''
+        // } // 有效配置
+        if (flag == 1) {
+          console.log("开始读取邮箱表");
+          for (let i = 2; i <= 2; i++) {
+            // 从工作表中读取推送数据
+            jsonEmail.server = Application.Range("A" + i).Text;
+            jsonEmail.port = Application.Range("B" + i).Text;
+            jsonEmail.sender = Application.Range("C" + i).Text;
+            jsonEmail.authorizationCode = Application.Range("D" + i).Text;
+            if (Application.Range("A" + i).Text == "") {
+              // 如果为空行，则提前结束读取
+              break;
+            }
+          }
+          // console.log(jsonEmail)
+        }
+        break;
+      }
+    }
+  }
+}
+
+// 推送钉钉机器人
+function dingtalk(message, key) {
+  let url = "https://oapi.dingtalk.com/robot/send?access_token=" + key;
+  let resp = HTTP.post(url, { msgtype: "text", text: { content: message } });
+  // console.log(resp.text())
+  sleep(5000);
+}
+// 推送Discord机器人
+function discord(message, key) {
+  let url = key;
+  let resp = HTTP.post(url, { content: message });
+  //console.log(resp.text())
+  sleep(5000);
+}
+function sleep(d) {
+  for (var t = Date.now(); Date.now() - t <= d; );
+}
+
+// 激活工作表函数
+function ActivateSheet(sheetName) {
+  let flag = 0;
+  try {
+    // 激活工作表
+    let sheet = Application.Sheets.Item(sheetName);
+    sheet.Activate();
+    console.log("激活工作表：" + sheet.Name);
+    flag = 1;
+  } catch {
+    flag = 0;
+    console.log("无法激活工作表，工作表可能不存在");
+  }
+  return flag;
+}
+
+// 对推送数据进行处理
+function jsonPushHandle(pushName, pushFlag, pushKey) {
+  let length = jsonPush.length;
+  for (let i = 0; i < length; i++) {
+    if (jsonPush[i].name == pushName) {
+      if (pushFlag == "是") {
+        jsonPush[i].flag = 1;
+        jsonPush[i].key = pushKey;
+      }
+    }
+  }
+}
+
+// cookie字符串转json格式
+function cookie_to_json(cookies) {
+  var cookie_text = cookies;
+  var arr = [];
+  var text_to_split = cookie_text.split(";");
+  for (var i in text_to_split) {
+    var tmp = text_to_split[i].split("=");
+    arr.push('"' + tmp.shift().trim() + '":"' + tmp.join(":").trim() + '"');
+  }
+  var res = "{\n" + arr.join(",\n") + "\n}";
+  return JSON.parse(res);
+}
+
+// 获取10 位时间戳
+function getts10() {
+  var ts = Math.round(new Date().getTime() / 1000).toString();
+  return ts;
+}
+
+// 获取13位时间戳
+function getts13(){
+  // var ts = Math.round(new Date().getTime()/1000).toString()  // 获取10 位时间戳
+  let ts = new Date().getTime()
+  return ts
+}
+
+// 符合UUID v4规范的随机字符串 b9ab98bb-b8a9-4a8a-a88a-9aab899a88b9
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0,
+        v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function getUUIDDigits(length) {
+    var uuid = generateUUID();
+    return uuid.replace(/-/g, '').substr(16, length);
+}
+ 
+
+
+// 获取sign，返回小写
+function getsign(data) {
+  var sign = Crypto.createHash("md5")
+    .update(data, "utf8")
+    .digest("hex")
+    // .toUpperCase() // 大写
+    .toString();
+  return sign;
+}
+
+
+// 具体的执行函数
+function execHandle(cookie, pos) {
+  let messageSuccess = "";
+  let messageFail = "";
+  let messageName = "";
+  // 推送昵称或单元格，还是不推送位置标识
+  if (messageNickname == 1) {
+    // 推送昵称或单元格
+    messageName = Application.Range("C" + pos).Text;
+    if(messageName == "")
+    {
+      messageName = "单元格A" + pos + "";
+    }
+  }
+
+  posLabel = pos-2 ;  // 存放下标，从0开始
+  messageHeader[posLabel] = messageName
+
+  // =================修改这块区域，区域开始=================
+
+  url1 = "http://www.jyeoo.com/api/SignPost"; // 签到url（修改这里，这里填抓包获取到的地址）
+
+  // （修改这里，这里填抓包获取header，全部抄进来就可以了，按照如下用引号包裹的格式，其中小写的cookie是从表格中读取到的值。）
+  headers= {
+    "Cookie": cookie,
+    // "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.70",
+  }
+
+  // （修改这里，这里填抓包获取data，全部抄进来就可以了，按照如下用引号包裹的格式。POST请求才需要这个，GET请求就不用它了）
+  data = {
+    // "csrf_token":"",
+  }
+  
+  // （修改这里，以下请求方式三选一即可)
+  // 请求方式1：POST请求，抓包的data数据格式是 {"aaa":"xxx","bbb":"xxx"} 。则用这个
+  resp = HTTP.post(
+    url1,
+    JSON.stringify(data),
+    { headers: headers }
+  );
+
+  // // 请求方式2：POST请求，抓包的data数据格式是 aaa=xxx&bbb=xxx 。则用这个
+  // resp = HTTP.post(
+  //   url1,
+  //   data,
+  //   { headers: headers }
+  // );
+
+  // // 请求方式3：GET请求，无data数据。则用这个
+  // resp = HTTP.get(
+  //   url1,
+  //   { headers: headers }
+  // );
+  
+  if (resp.status == 200) {
+    resp = resp.json();
+    console.log(resp)
+
+    // （修改这里，这里就是自己写了，根据抓包的响应自行修改）
+    // {"S":0,"M":"签到成功!已连续签到4天!<br/>奖励20积分","D”:0}
+    // {"S":1,"M":"今天已经签到，请明天继续","D":null}
+    // {"S":-2,"M":"请登录后操作","D":{}}
+    // 接收到的响应数据是json格式，如下，假设有2种情况
+    // 情况1：{"code": "0","message": "签到成功"}
+    // 情况2：{"code":"-1","message":"签到失败"}
+    respcode = resp["S"]           // 通过resp["键名"]的方式获取值.假设响应数据是情况1，则读取到数字“0”
+    // respmsg = resp["message"]  // 通过resp["键名"]的方式获取值，假设响应数据是情况1，这里取到的值就是“签到成功”
+    if(respcode == 0 || respcode == 1)       // 通过code值来判断是不是签到成功，由抓包的情况1知道，0代表签到成功了,所以让code与0比较
+    { 
+      // 这里是签到成功
+      respmsg = resp["M"] 
+      content = respmsg + " "//"签到成功 "  // // 给自己看的，双引号内可以随便写
+
+      messageSuccess += content;
+      console.log(content)
+    }
+    else
+    {
+      // 这里是签到失败
+      // msg = "签到失败 "   // 给自己看的，可以随便写，如 msg = "失败啦！ " 。
+      // content = msg + " "  
+      respmsg = resp["M"] 
+      content = respmsg + " "   
+      messageFail += content;
+      console.log(content)
+    }
+
+  } else {
+    content = "签到失败 "
+    messageFail += content;
+    console.log(content);
+  }
+
+  // =================修改这块区域，区域结束=================
+
+  sleep(2000);
+  if (messageOnlyError == 1) {
+    messageArray[posLabel] = messageFail;
+  } else {
+    messageArray[posLabel] = messageFail + " " + messageSuccess;
+  }
+
+  if(messageArray[posLabel] != "")
+  {
+    console.log(messageArray[posLabel]);
+  }
+}
